fc4afd4004cc8bbe2a53e56640a485e5d1e24b5b
arevalolance <54465849+arevalolance@users.noreply.github.com>
2023-01-18 14:15:01 +0800

    fix: SignIn directly to provider

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -49,7 +49,7 @@ const SignInModal = ({
             } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
             onClick={() => {
               setSignInClicked(true);
-              signIn();
+              signIn("spotify");
             }}
           >
             {signInClicked ? (
